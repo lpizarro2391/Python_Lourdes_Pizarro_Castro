99d1ed5a51834ddb9e2df594ab65f9bac1c6258d
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-07 08:53:12 -0600

    se empieza con importando modulos

--- billeteradigital.py
+++ billeteradigital.py
@@ -1,47 +1,9 @@
-import requests
-_ENDPOINT = "http://api.binance.com"
+#Para ejecutar se utiliza> python BilletaraDigital.py  (desde la consola)
+#Las cotizaciones se procesan y actualizan al principio de la aplicación para todas las monedas de la lista
+import random
+from requests import Request, session
+from requests.exceptions import ConnectionError, Timeout
+import json
+from datetime import datetime
 
-def _url(api):
-    return _ENDPOINT+api
 
-def get_price(cripto):
-    return requests.get(_url("/api/v3/ticker/price?symbol="+cripto))
-   
-def esmoneda(cripto):
-    criptos = ["BTC", "BCC","LTC","ETH","ETC","XRP"]
-    return  cripto in criptos
-
-def esnumero(numero):
-    return numero.replace('.','',1).isdigit()
-
-monedas=[]
-cantidades=[]
-cotizaciones=[]
-i=0
-while i<3:
-    moneda=input("Ingrese el nombre de la moneda: ")
-    while not esmoneda(moneda):
-        print("Moneda Invalida")
-        moneda=input("Ingrese el nombre de la moneda:")
-    else:
-        monedas.append(moneda)
-        data = get_price(moneda+"USDT").json()
-        cotizaciones.append(float(data["price"]))
-        cantidad =input("Indique la cantidad de "+moneda+": ")
-        while not esnumero(cantidad):
-            cantidad: input("Indique la cantidad de "+moneda+": ")
-        else: 
-            cantidades.append(float(cantidad))
-    i+=1
-
-i=0
-total=0
-
-while i<3:
-    total+=cantidades[i]*cotizaciones[i]
-    print("Moneda: ",monedas[i],
-    ",Cantidad: ",cantidades[i],
-    ",Cotizacion:",cotizaciones[i],
-    ",Cantidad de USDT",cantidades[i]*cotizaciones[i])
-    i+=1
-print("Total en USDT es:",str(total))
